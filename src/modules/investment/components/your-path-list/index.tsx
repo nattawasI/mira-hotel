e02bb352c49f167ca9a1f0684944427435a0b2f6
nattawas.i <nattawas.i@gmail.com>
2023-11-03 22:14:46 +0700

    save

--- src/modules/investment/components/your-path-list/index.tsx
+++ src/modules/investment/components/your-path-list/index.tsx
@@ -1,42 +1,50 @@
+/** libs */
+import { cln } from '@/libs/utils'
+
 /** components */
 import YourPathCard from '@/modules/investment/components/your-path-card'
+
+const dataList = [
+  {
+    id: 'discover',
+    title: '1. Discover',
+    description: 'Explore Mira’s investment concept and the available opportunities.',
+    srcImage: '/investment/discover.jpg',
+  },
+  {
+    id: 'evaluate',
+    title: '2. Evaluate',
+    description: 'Dive deeper into the specifics and quantify the potential.',
+    srcImage: '/investment/evaluate.jpg',
+  },
+  {
+    id: 'invest',
+    title: '3. Invest',
+    description: 'Take the leap and become a valued part of the global investment community.',
+    srcImage: '/investment/invest.jpg',
+  },
+  {
+    id: 'thrive',
+    title: '4. Thrive',
+    description: 'Witness your investments flourish and be an integral part of the Mira success story.',
+    srcImage: '/investment/thrive.jpg',
+  },
+]
 
 const YourPathList = (): JSX.Element => {
   return (
     <div className="content-container no-scrollbar overflow-x-auto">
       <div className="flex gap-x-1 lg:gap-x-2">
-        <div className="flex-shrink-0">
-          <YourPathCard
-            srcImage="/investment/discover.jpg"
-            title="1. Discover"
-            description="Explore Mira's investment concept and the available opportunities."
-            className="h-full"
-          />
-        </div>
-        <div className="flex-shrink-0">
-          <YourPathCard
-            srcImage="/investment/evaluate.jpg"
-            title="2. Evaluate"
-            description="Dive deeper into the specifics and quantify the potential."
-            className="h-full"
-          />
-        </div>
-        <div className="flex-shrink-0">
-          <YourPathCard
-            srcImage="/investment/invest.jpg"
-            title="3. Invest"
-            description="Take the leap and become a valued part of the global investment community."
-            className="h-full"
-          />
-        </div>
-        <div className="flex-shrink-0 pr-1.25">
-          <YourPathCard
-            srcImage="/investment/thrive.jpg"
-            title="4. Thrive"
-            description="Witness your investments flourish and be an integral part of the Mira success story."
-            className="h-full"
-          />
-        </div>
+        {dataList.map((item, index) => (
+          <div className={cln('flex-shrink-0', index === dataList.length - 1 ? 'pr-1.25' : '')} key={item.id}>
+            <YourPathCard
+              srcImage={item.srcImage}
+              title={item.title}
+              description={item.description}
+              className="h-full"
+            />
+          </div>
+        ))}
       </div>
     </div>
   )
